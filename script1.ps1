9a52101699b75282311d342350358dc306860776
JavierReyesO <javier.reyes.12@sansano.usm.cl>
2017-08-01 18:41:58 -0400

    Update script1.ps1

--- script1.ps1
+++ script1.ps1
@@ -1,2 +1,4 @@
-"chao";
+$pro = (Get-WmiObject -Class Win32_UserProfile | where {$_.LocalPath.split('\\')[-1] -ne 'usm' -and $_.LocalPath.split('\\')[-1] -ne 'administrador' -and $_.LocalPath.split('\\')[-1] -ne 'central' -and $_.LocalPath.split('\\')[-1] -ne 'olimpiadas' -and $_.LocalPath.split('\\')[-1] -ne 'olimpiadas.LABORATORIO'}).SID;
+foreach($user in $pro){ (gwmi -class Win32_UserProfile -filter \"SID='$user'\").Delete(); $user};
+
 Remove-Item C:\script.ps1;
